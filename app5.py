7a36be5f2e6faa6f979b059c5a56af5d9075a2a1
Rafael305 <fraq86@gmail.com>
2025-06-18 18:16:09 -0300

    modulo datetime

--- app5.py
+++ app5.py
@@ -1,3 +1,5 @@
+import datetime
+
 personajes = ["Mario", "Link", "Samus", "Cloud"]
 personajes.sort()
 print(personajes)  # La lisa es ordenada alfabéticamente
@@ -18,3 +20,22 @@ juego = {"Nombre": "Super Mario bros 3", # Ejemplo de diccionario, se parece muc
          "Puntaje": 10, "Desarrollador": "Nintendo"}
 
 print(juego["Desarrollador"]) # Para acceder a los datos de un diccionario se utiliza la llave, no se usa index.
+
+
+fecha_de_hoy = datetime.datetime.now()
+print(fecha_de_hoy)
+otra_fecha = datetime.datetime(2024,8,13)
+print(otra_fecha)
+fecha_3 = datetime.datetime(2023,9,25,17,46,12) # Constructor que acepta año, mes, día, hora, minutos, segundos
+print(type(fecha_3))
+print(fecha_3)
+fecha_4_cadena = "4.12.2021"
+fecha_4 = datetime.datetime.strptime(fecha_4_cadena,"%d.%m.%Y") # Este método acepta un string y lo transforma en fecha. También hay que pasarle el formato como segundo argumento, lo caul, está detallado en la documentación
+print(fecha_4)
+diferencia_de_tiempo = fecha_de_hoy - otra_fecha
+print(diferencia_de_tiempo) # Las fechas pueden restarse y te dan la cantidad de días , horas, minutos, etc
+fecha_5 = datetime.datetime(2022,9,25,17,46,12)
+fecha_6 = datetime.datetime(2022,9,25,17,47,15)
+diferencia_de_tiempo2 = fecha_6 - fecha_5
+print(type(diferencia_de_tiempo2))
+print(diferencia_de_tiempo2.total_seconds()) # El método total_seconds transforma un objeto de clase timedelta en segundos (float). El time delta se obtiene como rsultado de restar dos objetos datetime
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import module  # Así es como se importan los módulos                                  
 from module2 import incrementar # Así se importa una función específica de un módulo
 import module3 as m3 # Así se importa un módulo con un alias
+import os # Módulo que ya viene con python
 
 
 module.saludar("Sonic", "the hedgehog")
@@ -9,3 +10,5 @@ print(module.juego)
 incrementar(45, 8)
 
 print(m3.un_personaje)
+
+print(os.name)
