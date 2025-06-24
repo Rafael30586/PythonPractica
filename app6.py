5d62b56e24522a2002400e3f0275a129f813e835
Rafael305 <fraq86@gmail.com>
2025-06-24 18:27:19 -0300

    herencia

--- app6.py
+++ app6.py
@@ -9,3 +9,26 @@ print(esta2)
 print(no_esta)
 
 # print("Hola"*3.65) da error.
+
+juego1 = "Halo combat evolved"
+juego2 = "Sonic 3"
+
+print(juego1,juego2) # Ejemplo de cómo se "concatenan" strings al usar la función print()
+
+
+class Vehiculo:
+    def __init__(self):
+        pass
+
+
+class Moto(Vehiculo): # La clase moto hereda de la clase vehiculo
+    def __init__(self):
+        super().__init__()
+
+class Vehiculo_Electrico:
+    def __init__(self):
+        pass
+
+class Bicicleta_Electrica(Vehiculo,Vehiculo_Electrico): # Ejemplo de herencia múltiple en python
+    def __init__(self): # Hereda el constructor de la clase Vehiculo porque se le da preferencia al primero
+        super().__init__()
