2a2c10ae28022a953c9786713a807c559f0b67a5
Rafael305 <fraq86@gmail.com>
2025-06-24 16:39:13 -0300

    agregado de frame

--- InterfacesGraficas/tkinter_prueba.py
+++ InterfacesGraficas/tkinter_prueba.py
@@ -3,8 +3,16 @@ import tkinter
 raiz = tkinter.Tk(screenName="ventana")
 raiz.title("Ventana")
 raiz.resizable(1,1)
-raiz.geometry("300x400")
+# raiz.geometry("300x400") La raíz se adaptará al tamaño del contenedor interno (mi_frame en este caso)
+raiz.config(bg="#0e74b3")
 
+mi_frame = tkinter.Frame()
+
+mi_frame.pack(side="left",anchor="n") # Los parámetros side y anchor sirven para definir como va a quedar el frame en caso de agrandar el tamaño de la raíz
+
+mi_frame.config(bg="#80e352")
+
+mi_frame.config(width="300", height="400")
 
 raiz.mainloop()
 
---
+++ InterfacesGraficas/tkinter_prueba2.pyw
@@ -0,0 +1,10 @@
+import tkinter 
+
+raiz = tkinter.Tk(screenName="ventana")
+raiz.title("Ventana")
+raiz.resizable(1,1)
+raiz.geometry("700x400")
+raiz.config(bg="#dbfbb1")
+
+
+raiz.mainloop()
